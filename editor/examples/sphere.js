045bdd8ecb2e4a6161e6cd845991f85829dcffad
rye terrell <ryterrell@ryeterrell.net>
2014-08-01 07:48:12 -0500

    center sphere example

--- editor/examples/sphere.js
+++ editor/examples/sphere.js
@@ -8,9 +8,9 @@ setCamera(0, 0.713, 27.45, radius, radius, radius);
 for (var x = 0; x < radius * 2; x++) {
     for (var y = 0; y < radius * 2; y++) {
         for (var z = 0; z < radius * 2; z++) {
-            var dx = radius - x;
-            var dy = radius - y;
-            var dz = radius - z;
+            var dx = radius - x - 0.5;
+            var dy = radius - y - 0.5;
+            var dz = radius - z - 0.5;
             var d = Math.sqrt(dx*dx + dy*dy + dz*dz);
             if (d <= radius) {
                 set(x, y, z, 0, 0.5, 1);
