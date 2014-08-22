af59bf3c921ac55595576ef60ffab71b0176a540
rye terrell <ryterrell@ryeterrell.net>
2014-08-21 21:50:36 -0500

    better cave demo

--- demos/cave/main.js
+++ demos/cave/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var scene, camera, renderer, light, rock;
+var scene, camera, renderer, light, rock, dummy;
 var size = 8;
-var range = 8;
+var range = 2;
 
 window.onload = function() {
     var renderCanvas = document.getElementById("render-canvas");
@@ -16,7 +16,7 @@ window.onload = function() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100);
     camera.position.set(0, 0, 32);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
@@ -24,7 +24,24 @@ window.onload = function() {
     light.position = camera.position.clone();
     scene.add(light);
 
-    var rock = new Brownie(renderer);
+    buildRock();
+
+    rock.geom = rock.getGeometry();
+    rock.mat = new THREE.MeshPhongMaterial({
+        vertexColors: THREE.VertexColors,
+        specular: 0
+    });
+
+    dummy = new THREE.Object3D();
+    scene.add(dummy);
+
+    window.onresize = onResize;
+
+    animate();
+}
+
+function buildRock() {
+    rock = new Brownie(renderer);
     for (var x = 0; x < size; x++) {
         for (var y = 0; y < size; y++) {
             for (var z = 0; z < size; z++) {
@@ -36,8 +53,8 @@ window.onload = function() {
     for (var x = 0; x < size; x++) {
         for (var y = 0; y < size; y++) {
             for (var z = 0; z < size; z++) {
-                if (x == 0 || x == size - 1 || y == 0 || y == size - 1 || z == 0 || z == size - 1) {
-                    if (Math.random() < 0.25) {
+                if (x == 0 || x == size - 1 || y == 0 || y == size - 1 || z == 1 || z == size - 1) {
+                    if (Math.random() < 0.125) {
                         rock.unset(x, y, z);
                     }
                 }
@@ -45,26 +62,61 @@ window.onload = function() {
         }
     }
     rock.rebuild();
-    var geometry = rock.getGeometry();
-    var material = new THREE.MeshPhongMaterial({
-        vertexColors: THREE.VertexColors,
-        specular: 0
-    });
-    for (var x = -range; x <= range; x++) {
-        for (var y = -range; y <= range; y++) {
-            var p = new THREE.Mesh(geometry, material);
-            p.position.set(-size/2, -size/2, -size/2);
-            var m = new THREE.Object3D();
-            m.add(p);
-            var z = Math.random() < 0.5 ? 1 : 0;
-            m.position.set(x * size, y * size, z * size);
-            scene.add(m);
+}
+
+var plots = {};
+
+function getPlot(x, y) {
+    if (!plots[[x, y]]) {
+        var plot = [];
+        if (Math.random() < 0.5) {
+            plot.push("rock");
+        } else {
+            plot.push("wall");
         }
+        plots[[x, y]] = plot;
     }
+    return plots[[x, y]];
+}
 
-    window.onresize = onResize;
+var meshpool = [];
+function getRockMesh() {
+    if (meshpool.length === 0) {
+        var p = new THREE.Object3D();
+        var m = new THREE.Mesh(rock.geom, rock.mat);
+        m.position.set(-size / 2, -size / 2, -size / 2);
+        p.add(m);
+        meshpool.push(p);
+    }
+    return meshpool.pop();
+}
 
-    animate();
+function releaseRockMesh(m) {
+    meshpool.push(m);
+}
+
+function getPlotMesh(x, y) {
+    var mesh = new THREE.Object3D();
+    var plot = getPlot(x, y);
+    if (_.contains(plot, "wall")) {
+        var m = getRockMesh();
+        m.position.set(x * size, y * size, 0);
+        mesh.add(m);
+    }
+    if (_.contains(plot, "rock")) {
+        var m = getRockMesh();
+        m.position.set(x * size, y * size, size);
+        mesh.add(m);
+    }
+    return mesh;
+}
+
+function releasePlotMesh(pm) {
+    var children = pm.children.slice();
+    for (var i in children) {
+        var b = children[i];
+        releaseRockMesh(b);
+    }
 }
 
 function onResize() {
@@ -73,13 +125,60 @@ function onResize() {
     camera.updateProjectionMatrix();
 }
 
-var tick = 0;
+function keyOn(key) {
+    return _.contains(KeyboardJS.activeKeys(), key)
+}
+
+function getPlayerPlot() {
+    return {
+        x: Math.round(camera.position.x / size),
+        y: Math.round(camera.position.y / size)
+    }
+}
+
+function clearScene() {
+    var children = dummy.children.slice();
+    for (var i in children) {
+        var b = children[i];
+        dummy.remove(b);
+        releasePlotMesh(b);
+    }
+}
+
+function drawScene() {
+    var pp = getPlayerPlot();
+    for (var x = pp.x - range; x <= pp.x + range; x++) {
+        for (var y = pp.y - range; y <= pp.y + range; y++) {
+            var m = getPlotMesh(x, y);
+            dummy.add(m);
+        }
+    }
+}
+
+var speed = 0.25;
+
+function updatePlayerPosition() {
+    if (keyOn('up') || keyOn('w')) {
+        camera.position.y += speed;
+    }
+    if (keyOn('down') || keyOn('s')) {
+        camera.position.y -= speed;
+    }
+    if (keyOn('left') || keyOn('a')) {
+        camera.position.x -= speed;
+    }
+    if (keyOn('right') || keyOn('d')) {
+        camera.position.x += speed;
+    }
+}
+
 function animate() {
-    tick++;
-    var r = size * range;
-    camera.position.set(r * Math.cos(tick * 0.002), r * Math.sin(tick * 0.002), size * 4)
-    camera.lookAt(new THREE.Vector3(0, 0, -size * 4));
-    light.position.set(r * Math.cos(tick * 0.002), r * Math.sin(tick * 0.002), size * 2)
+    updatePlayerPosition();
+    clearScene();
+    console.log(meshpool.length);
+    drawScene();
+    light.position = camera.position.clone();
+    light.position.z = 8;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
